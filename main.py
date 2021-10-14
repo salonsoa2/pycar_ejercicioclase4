098a49a0028dacf1d255c734b6106122b525c4dc
salonsoa2 <salonsoa2@alumnos.nebrija.es>
2021-10-14 11:44:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import a library of functions called 'pygame'
+import pygame
+from main_functions import *
+
+# Initialize the game engine
+pygame.init()
+
+#call main routine
+ancho = int(input("ancho de la ventana: "))
+alto = int(input("alto de la ventana: "))
+size = (ancho, alto)
+titulo = input("titulo simulador: ")
+main2(size, titulo)
